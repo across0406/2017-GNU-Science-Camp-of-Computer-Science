5aefcfbaadf2ebbf875e65452d1d509640d274f9
Dong hyeon Kim <across0406@nate.com>
2017-07-20 01:38:53 +0900

    Add variable.py and modify data_type.py

--- python_basis/data_type.py
+++ python_basis/data_type.py
@@ -1,4 +1,4 @@
-print(type(9))
+print(type(10))
 print(type(10.0))
-print(type(11.1))
 print(type("Hello"))
+print(type(True))
---
+++ python_basis/variable.py
@@ -0,0 +1,4 @@
+# format:[variable's name] = value
+num = 2017
+print('value of num: ', num)
+print('type of num: ', type(num))
