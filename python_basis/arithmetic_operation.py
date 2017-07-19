ee827eb69a8aa8c1a545e72f9113f5d100d44550
Dong hyeon Kim <across0406@nate.com>
2017-07-20 01:16:11 +0900

    Add hello_world.py, arithmetic_operation.py, and data_type.py

---
+++ python_basis/arithmetic_operation.py
@@ -0,0 +1,7 @@
+print(1 + 2)
+print(1 - 2)
+print(3 * 4)
+print(5 / 6)
+print(7 ** 8)
+print(9 % 10)
+print(10 % 9)
---
+++ python_basis/data_type.py
@@ -0,0 +1,4 @@
+print(type(9))
+print(type(10.0))
+print(type(11.1))
+print(type("Hello"))
